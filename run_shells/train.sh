916c42fdb302a291e518a190007a42208f2333db
aijianiula0601@gmail.com <348348jia>
2024-06-05 08:39:31 +0800

    update

--- run_shells/train.sh
+++ run_shells/train.sh
@@ -6,18 +6,20 @@ curdir=$(pwd)
 echo "curdir:$curdir"
 cd "$curdir" || exit
 
+#-------------------------
 #解压所有文件
-bash unzip_images.sh
+#-------------------------
+#bash unzip_images.sh
 
-cd ../
-
-echo "----:$(pwd)"
 
 data_path='/mnt/cephfs/hjh/common_dataset/images/imagenet/ILSVRC2012_img_train'
 results_dir='/mnt/cephfs/hjh/train_record/images/DiT/training'
+cd ../
 
+#-------------------------
+# 训练
+#-------------------------
 your_random_port=65036
-
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
   torchrun --nproc_per_node=8 --master_port=${your_random_port} train.py \
   --data-path ${data_path} \
