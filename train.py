6a03a81e92ebda600145556f8f37b1e4a9ef0849
aijianiula0601@gmail.com <348348jia>
2024-07-31 09:24:18 +0800

    update

--- train.py
+++ train.py
@@ -107,6 +107,24 @@ def center_crop_arr(pil_image, image_size):
     return Image.fromarray(arr[crop_y: crop_y + image_size, crop_x: crop_x + image_size])
 
 
+class ImageFolderWithPaths(ImageFolder):
+    def __getitem__(self, index):
+        try:
+            # This is what ImageFolder normally returns
+            original_tuple = super(ImageFolderWithPaths, self).__getitem__(index)
+
+            # Get the image path
+            path = self.imgs[index][0]
+
+            # Make a new tuple that includes the original and the path
+            tuple_with_path = (original_tuple[0], original_tuple[1], path)
+            return tuple_with_path
+
+        except (IOError, OSError, Image.DecompressionBombError) as e:
+            print(f"Skipping image at index {index}, path {self.imgs[index][0]}: {e}")
+            return None
+
+
 #################################################################################
 #                                  Training Loop                                #
 #################################################################################
@@ -168,7 +186,9 @@ def main(args):
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True)
     ])
-    dataset = ImageFolder(args.data_path, transform=transform)
+    # dataset = ImageFolder(args.data_path, transform=transform)
+    dataset = ImageFolderWithPaths(args.data_path, transform=transform)
+
     sampler = DistributedSampler(
         dataset,
         num_replicas=dist.get_world_size(),
@@ -202,7 +222,7 @@ def main(args):
     for epoch in range(args.epochs):
         sampler.set_epoch(epoch)
         logger.info(f"Beginning epoch {epoch}...")
-        for x, y in loader:
+        for x, y, _ in loader:
             x = x.to(device)
             y = y.to(device)
             with torch.no_grad():
